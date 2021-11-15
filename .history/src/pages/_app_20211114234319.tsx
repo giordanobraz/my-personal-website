de6b458fb94b96bee6b539769bdad15175b967a0
Giordano <giordanosi2011@gmail.com>
2021-11-14 23:52:21 -0300

    c'mon

---
+++ .history/src/pages/_app_20211114234319.tsx
@@ -0,0 +1,36 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import "../../public/prism/prism.css";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import "../styles/globals.scss";
+import { theme } from "../styles/theme";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const iframely = `document.querySelectorAll( 'oembed[url]' ).forEach( element => {
+    iframely.load( element, element.attributes.url.value );
+  } );`;
+  const gtag = `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-NNQMNNCNE7');`;
+  return (
+    <ChakraProvider theme={theme}>
+      <Script id="iframely">{iframely}</Script>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-NNQMNNCNE7`}
+      />
+      <Script id="gtag" strategy="lazyOnload">
+        {gtag}
+      </Script>
+      <Script src={"/prism/prism.js"}></Script>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
