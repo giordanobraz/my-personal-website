40672840fe3a0ca8294f92e9c4b27726ef741284
Giordano <giordanosi2011@gmail.com>
2022-06-18 23:19:37 -0300

    blog is back

--- src/pages/blog/post/[slug].tsx
+++ src/pages/blog/post/[slug].tsx
@@ -1,4 +1,9 @@
-import { Flex, Text } from "@chakra-ui/layout";
+import { Box, Heading, Img, Stack, VStack } from "@chakra-ui/react";
+import { getAllPosts, getPostBySlug } from "../../../services/strapi";
+
+import Layout from "../../../components/blog/blogLayout";
+import { NextSeo } from "next-seo";
+import { useRouter } from "next/router";
 
 interface Params {
   params: {
@@ -36,118 +41,113 @@ interface PostProps {
 }
 
 export default function Post({ post, formatted_date }: PostProps) {
+  const { isFallback } = useRouter();
+
+  const seo = {
+    title: `${post.attributes.title} | Giordano Bruno - Desenvolvedor`,
+    description: post.attributes.description,
+    openGraph: {
+      type: "website",
+      url: `"https://giordano.dev.br/blog/post/${post.attributes.slug}"`,
+      title: `${post.attributes.title}`,
+      description: `${post.attributes.description}`,
+      site_name: "Giordano Bruno - Desenvolvedor",
+      images: [
+        {
+          url: `${post.attributes.image?.data.attributes.url}`,
+          alt: `${post.attributes.title}`,
+        },
+      ],
+    },
+  };
+
+  if (isFallback) {
+    return <span>Carregando...</span>;
+  }
+
+  function createMarkup(content: any) {
+    return {
+      __html: content,
+    };
+  }
+
   return (
-    <Flex>
-      <Text>{"I'll be back"}</Text>
-    </Flex>
+    <>
+      <NextSeo {...seo} />
+
+      <Layout>
+        <Stack paddingTop={"70px"} flexDir={"column"} spacing={5}>
+          <Img
+            src={`${post.attributes.image?.data.attributes.url}`}
+            alt={post.attributes.title}
+            maxH={500}
+            loading="lazy"
+            objectFit="cover"
+            id="top"
+          />
+
+          <Stack minWidth={"100%"}>
+            <VStack maxWidth={"900px"} margin={"0 auto"}>
+              <Heading fontSize={"md"} color="grey.500" textAlign="left">
+                {formatted_date}
+              </Heading>
+              <Heading
+                fontSize={["2xl", "3xl", "6xl"]}
+                textTransform="uppercase"
+                textAlign={"center"}
+              >
+                {post.attributes.title}
+              </Heading>
+              <Box fontSize={["sm", "md"]} color="grey.300" fontFamily="Roboto">
+                <div
+                  className="ck-content article"
+                  dangerouslySetInnerHTML={createMarkup(
+                    post.attributes.content
+                  )}
+                />
+              </Box>
+            </VStack>
+          </Stack>
+        </Stack>
+      </Layout>
+    </>
   );
-  // const { isFallback } = useRouter();
-
-  // const seo = {
-  //   title: `${post.attributes.title} | Giordano Bruno - Desenvolvedor`,
-  //   description: post.attributes.description,
-  //   openGraph: {
-  //     type: "website",
-  //     url: `"https://giordano.dev.br/blog/post/${post.attributes.slug}"`,
-  //     title: `${post.attributes.title}`,
-  //     description: `${post.attributes.description}`,
-  //     site_name: "Giordano Bruno - Desenvolvedor",
-  //     images: [
-  //       {
-  //         url: `${post.attributes.image?.data.attributes.url}`,
-  //         alt: `${post.attributes.title}`,
-  //       },
-  //     ],
-  //   },
-  // };
-
-  // if (isFallback) {
-  //   return <span>Carregando...</span>;
-  // }
-
-  // function createMarkup(content: any) {
-  //   return {
-  //     __html: content,
-  //   };
-  // }
-
-  // return (
-  //   <>
-  //     <NextSeo {...seo} />
-
-  //     <Layout>
-  //       <Stack paddingTop={"70px"} flexDir={"column"} spacing={5}>
-  //         <Img
-  //           src={`${post.attributes.image?.data.attributes.url}`}
-  //           alt={post.attributes.title}
-  //           maxH={500}
-  //           loading="lazy"
-  //           objectFit="cover"
-  //           id="top"
-  //         />
-
-  //         <Stack minWidth={"100%"}>
-  //           <VStack maxWidth={"900px"} margin={"0 auto"}>
-  //             <Heading fontSize={"md"} color="grey.500" textAlign="left">
-  //               {formatted_date}
-  //             </Heading>
-  //             <Heading
-  //               fontSize={["2xl", "3xl", "6xl"]}
-  //               textTransform="uppercase"
-  //               textAlign={"center"}
-  //             >
-  //               {post.attributes.title}
-  //             </Heading>
-  //             <Box fontSize={["sm", "md"]} color="grey.300" fontFamily="Roboto">
-  //               <div
-  //                 className="ck-content article"
-  //                 dangerouslySetInnerHTML={createMarkup(
-  //                   post.attributes.content
-  //                 )}
-  //               />
-  //             </Box>
-  //           </VStack>
-  //         </Stack>
-  //       </Stack>
-  //     </Layout>
-  //   </>
-  // );
 }
 
-// export async function getStaticProps({ params }: Params) {
-//   const { slug } = params;
-//   const response = await getPostBySlug(slug);
-//   const post = response.data[0];
+export async function getStaticProps({ params }: Params) {
+  const { slug } = params;
+  const response = await getPostBySlug(slug);
+  const post = response.data[0];
 
-//   const formatted_date = new Date(
-//     post.attributes.publishedAt
-//   ).toLocaleDateString("pt-BR", {
-//     day: "2-digit",
-//     month: "long",
-//     year: "numeric",
-//   });
+  const formatted_date = new Date(
+    post.attributes.publishedAt
+  ).toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
 
-//   return {
-//     props: {
-//       post,
-//       formatted_date,
-//     },
-//     revalidate: 60,
-//   };
-// }
+  return {
+    props: {
+      post,
+      formatted_date,
+    },
+    revalidate: 60,
+  };
+}
 
-// export async function getStaticPaths() {
-//   const getAllDataFromStrapi = await getAllPosts();
-//   const posts = getAllDataFromStrapi.data;
+export async function getStaticPaths() {
+  const getAllDataFromStrapi = await getAllPosts();
+  const posts = getAllDataFromStrapi.data;
 
-//   return {
-//     fallback: false,
-//     paths: posts.map((post: any) => {
-//       return {
-//         params: {
-//           slug: post.attributes.slug,
-//         },
-//       };
-//     }),
-//   };
-// }
+  return {
+    fallback: false,
+    paths: posts.map((post: any) => {
+      return {
+        params: {
+          slug: post.attributes.slug,
+        },
+      };
+    }),
+  };
+}
